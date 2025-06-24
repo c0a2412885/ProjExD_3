19ff0abdef42748fdef3b4f9a9d012693cf1ce71
c0a2412885 <c0a2412885@edu.teu.ac.jp>
2025-06-24 14:37:46 +0900

    ゲームオーバー画面

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -167,6 +167,9 @@ def main():
 
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
+                moji = pg.font.Font(None, 80)
+                text = moji.render("Game Over", True, (255, 0, 0))
+                screen.blit(text, [WIDTH//2-150, HEIGHT//2])
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
